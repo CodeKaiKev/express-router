9d94bcff40cab1408dbbcf5d4f0d33b103bbde8c
Codekaikev <codekaikev@gmail.com>
2023-02-23 11:46:07 +0000

    Hi

--- server.js
+++ server.js
@@ -2,51 +2,17 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-// List of Users
-let users = [
-    {
-        name: "User 1",
-        age: 30
-    },
-    {
-        name: "User 2",
-        age: 45
-    },
-    {
-        name: "User 3",
-        age: 27
-    },
-    {
-        name: "User 4",
-        age: 22
-    }
-]
 
-// List of Fruits
-let fruits = [
-    {
-        name: "Apple",
-        color: "Red"
-    },
-    {
-        name: "Banana",
-        color: "Yellow"
-    },
-    {
-        name: "Kiwi",
-        color: "Green"
-    },
-    {
-        name: "Grape",
-        color: "Purple"
-    },
-]
 
 // Express Routes
 
+const fruitRouter = require("./fruits/fruits");
+const userRouter = require("./users/users");
 
+app.use("/fruits", fruitRouter);
+app.use("/users", userRouter);
 
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+    console.log(`App listening on port http://localhost:${port}/users`)
 })
